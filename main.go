342beb94428ab53a881db9a33cbfe1f045e8cf98
Haim <adsyandex@zohomail.com>
2025-01-10 09:38:03 +1000

    Создал ещё одну ветку(dev) и внёс изменение в main.go
    # Conflicts:
    #	main.go

--- main.go
+++ main.go
@@ -1,23 +1,7 @@
 package main
 
-import (
-    "fmt"
-)
+import "fmt"
 
 func main() {
-    var size int
-    fmt.Print("Введите размер доски: ")
-    fmt.Scan(&size)
-
-    for i := 0; i < size; i++ {
-        for j := 0; j < size; j++ {
-            if (i+j)%2 == 0 {
-                fmt.Print(" ")
-            } else {
-                fmt.Print("#")
-            }
-        }
-        fmt.Println()
-    }
+	fmt.Println("Hello Go Developmen in Otus!")
 }
-
