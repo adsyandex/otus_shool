695c23fe4d3973799999aac38a84a6cad706087a
Haim <adsyandex@zohomail.com>
2025-01-10 11:40:37 +1000

    Update main.go
    Создаю chessboard

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+)
 
 func main() {
-	fmt.Println("Hello, Vitaliy, Go Development, student at Otus!")
+    var size int
+    fmt.Print("Введите размер доски: ")
+    fmt.Scan(&size)
+
+    for i := 0; i < size; i++ {
+        for j := 0; j < size; j++ {
+            if (i+j)%2 == 0 {
+                fmt.Print(" ")
+            } else {
+                fmt.Print("#")
+            }
+        }
+        fmt.Println()
+    }
 }
+
