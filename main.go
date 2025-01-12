a7e491dbc6a1ee16cfc7c07bd4adcef6613b8003
Haim <adsyandex@zohomail.com>
2025-01-12 03:14:55 +0000

    добавил код шахматная доска

--- main.go
+++ main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+// createChessBoard создает шахматную доску заданного размера.
+func createChessBoard(size int) string {
+	board := ""
+	for row := 0; row < size; row++ {
+		for col := 0; col < size; col++ {
+			if (row+col)%2 == 0 {
+				board += " " // Белая клетка
+			} else {
+				board += "#" // Чёрная клетка
+			}
+		}
+		board += "\n" // Переход на новую строку
+	}
+	return board
+}
 
 func main() {
-	fmt.Println("Hello World!")
+	var size int
+	fmt.Print("Введите размер шахматной доски: ")
+	fmt.Scan(&size)
+	fmt.Println(createChessBoard(size))
 }
+
