095b97282082a0efb75073174ed1ce0339bcf6b1
Haim <adsyandex@zohomail.com>
2025-01-29 01:22:39 +0000

    changed main file

--- main.go
+++ main.go
@@ -1,25 +1,28 @@
 package main
 
 import (
-	"fmt"
+    "fmt"
 )
 
 // createChessBoard создает шахматную доску заданного размера.
 func createChessBoard(size int) string {
-	board := ""
-	for row := 0; row < size; row++ {
-		for col := 0; col < size; col++ {
-			if (row+col)%2 == 0 {
-				board += " " // Белая клетка
-			} else {
-				board += "#" // Чёрная клетка
-			}
-		}
-		board += "\n" // Переход на новую строку
-	}
-	return board
+    board := ""
+    for row := 0; row < size; row++ {
+        for col := 0; col < size; col++ {
+            if (row+col)%2 == 0 {
+                board += " " // Белая клетка
+            } else {
+                board += "#" // Чёрная клетка
+            }
+        }
+        board += "\n" // Переход на новую строку
+    }
+    return board
 }
 
 func main() {
-	fmt.Println("Hello Vitaliy, Go Development, student at Otus!")
+    var size int
+    fmt.Print("Введите размер шахматной доски: ")
+    fmt.Scan(&size)
+    fmt.Println(createChessBoard(size))
 }
