9fc373cde470f5f54d52acc11a6e9facf82431f5
Haim <adsyandex@zohomail.com>
2025-02-27 12:48:33 +1000

    Update main.go
    Fixed the main package

--- todo/cmd/todo-app/main.go
+++ todo/cmd/todo-app/main.go
@@ -1,33 +1,30 @@
 package main
 
 import (
-    "fmt"
-    "os"
-    "github.com/gin-gonic/gin"
-    "github.com/adsyandex/otus_shool/internal/api" // Исправленный импорт
-    "github.com/adsyandex/otus_shool/internal/menu"
-    "github.com/adsyandex/otus_shool/internal/storage"
-    "github.com/adsyandex/otus_shool/internal/task"
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"github.com/adsyandex/otus_shool/internal/api"
+	"github.com/adsyandex/otus_shool/internal/logger"
+	"github.com/adsyandex/otus_shool/internal/storage"
+	"github.com/adsyandex/otus_shool/internal/task"
 )
 
 func main() {
-    // Инициализация хранилища
-    store := storage.NewFileStorage("tasks.json")
+	// Инициализация хранилища
+	store := storage.NewFileStorage("tasks.json")
 
-    // Инициализация менеджера задач
-    taskManager := task.NewTaskManager(store)
+	// Инициализация менеджера задач
+	taskManager := task.NewTaskManager(store)
 
-    // Выбор режима работы
-    if len(os.Args) > 1 && os.Args[1] == "--api" {
-        // Сетевой режим
-        r := gin.Default()
-        api.SetupRoutes(r, taskManager)
-        fmt.Println("Сервер запущен на http://localhost:8080")
-        r.Run(":8080")
-    } else {
-        // Консольный режим
-        appMenu := menu.NewMenu(taskManager)
-        fmt.Println("Добро пожаловать в TODO-лист!")
-        appMenu.Run()
-    }
+	// Инициализация логирования
+	consoleLogger := &logger.ConsoleLogger{}
+
+	// Запуск логирования в горутине
+	go logger.StartLogging(taskManager, consoleLogger)
+
+	// Запуск API-сервера
+	r := gin.Default()
+	api.SetupRoutes(r, taskManager)
+	fmt.Println("Сервер запущен на http://localhost:8080")
+	r.Run(":8080")
 }
