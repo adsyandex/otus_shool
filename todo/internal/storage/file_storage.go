63cb8e538591c3297c8b7afcdfcb1ab20493d47f
Haim <adsyandex@zohomail.com>
2025-02-28 11:41:09 +1000

    Update file_storage.go
    Add mutex

--- todo/internal/storage/file_storage.go
+++ todo/internal/storage/file_storage.go
@@ -3,12 +3,15 @@ package storage
 import (
 	"encoding/json"
 	"os"
+	"sync"
+
 	"github.com/adsyandex/otus_shool/internal/task"
 )
 
 // FileStorage реализует Storage с использованием файла
 type FileStorage struct {
 	filename string
+	mu       sync.Mutex // Добавляем мьютекс для защиты доступа к файлу
 }
 
 // NewFileStorage создает новый экземпляр FileStorage
@@ -16,8 +19,11 @@ func NewFileStorage(filename string) *FileStorage {
 	return &FileStorage{filename: filename}
 }
 
-// Save сохраняет задачу в файл
+// Save сохраняет задачу в файл (безопасно для нескольких горутин)
 func (fs *FileStorage) Save(task task.Task) error {
+	fs.mu.Lock()
+	defer fs.mu.Unlock()
+
 	tasks, err := fs.Load()
 	if err != nil {
 		return err
@@ -33,8 +39,11 @@ func (fs *FileStorage) Save(task task.Task) error {
 	return os.WriteFile(fs.filename, data, 0644)
 }
 
-// Load загружает задачи из файла
+// Load загружает задачи из файла (безопасно для нескольких горутин)
 func (fs *FileStorage) Load() ([]task.Task, error) {
+	fs.mu.Lock()
+	defer fs.mu.Unlock()
+
 	data, err := os.ReadFile(fs.filename)
 	if err != nil {
 		if os.IsNotExist(err) {
